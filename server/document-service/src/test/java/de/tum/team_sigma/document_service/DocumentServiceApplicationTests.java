65ad27bd373837ed56ffc02cd6d19e153c342dbc
simon huang <simon.huang@outlook.de>
2025-06-18 12:05:44 +0200

    try fix build

--- server/document-service/src/test/java/de/tum/team_sigma/document_service/DocumentServiceApplicationTests.java
+++ server/document-service/src/test/java/de/tum/team_sigma/document_service/DocumentServiceApplicationTests.java
@@ -2,9 +2,23 @@ package de.tum.team_sigma.document_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import io.minio.MinioClient;
+import io.weaviate.client.WeaviateClient;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 
 @SpringBootTest
 class DocumentServiceApplicationTests {
+
+	// Stub external service clients to avoid network calls during context load
+	@MockBean
+	private MinioClient minioClient;
+
+	@MockBean
+	private WeaviateClient weaviateClient;
+
+	@MockBean
+	private EmbeddingModel embeddingModel;
 
 	@Test
 	void contextLoads() {

36081be4cdb8bb8d2e390ef6237e53d4c3d761da
simon huang <simon.huang@outlook.de>
2025-07-19 11:04:11 +0200

    add t3-env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,6 +13,7 @@ scan({
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { env } from "./env";
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -27,7 +28,7 @@ declare module "@tanstack/react-router" {
 const queryClient = new QueryClient();
 
 // Get Clerk publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
 }
